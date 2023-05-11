d61bc6820ed7e2ca441798a986f5eb9798ab68d6
Makkinni <finmakmislgood@gmail.com>
2023-05-11 18:33:05 +0300

    Made Pagination and Search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
-import logoSvg from '../assets/img/pizza-logo.svg'
+import logoSvg from '../assets/img/pizza-logo.svg';
 import { Link } from 'react-router-dom';
-function Header() {
+import Search from './Search';
+function Header({ searchValue, setSearchValue }) {
   return (
     <div className="header">
       <div className="container">
         <Link to="/">
-        <div className="header__logo">
-          <img width="38" src={logoSvg} alt="Pizza logo" />
-          <div>
-            <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+          <div className="header__logo">
+            <img width="38" src={logoSvg} alt="Pizza logo" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
           </div>
-        </div>
         </Link>
+        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import axios from 'axios';
-
+import Pagination from '../components/Pagination';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -20,17 +21,20 @@ const Home = () => {
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sortType.sortProperty.replace('-', '');
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
     axios
       .get(
-        `https://6458ad9e4eb3f674df7a0c6e.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+        `https://6458ad9e4eb3f674df7a0c6e.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
       )
       .then((res) => {
         setItems(res.data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const skeletons = [...new Array(8)].map((_, index) => <Skeleton key={index} />);
   return (
     <div className="container">
       <div className="content__top">
@@ -38,11 +42,8 @@ const Home = () => {
         <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
