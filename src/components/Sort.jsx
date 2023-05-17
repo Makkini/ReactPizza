ce7da0272328ab7112241b776487266821fe50cc
Makkinni <finmakmislgood@gmail.com>
2023-05-17 18:37:41 +0300

    cart redux + hide popup on click body

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -12,14 +12,26 @@ export const list = [
 function Sort() {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
+  const sortRef = React.useRef();
   const [open, setOpen] = React.useState(false);
 
   const onClickSort = (obj) => {
-    dispatch(setSort(obj))
+    dispatch(setSort(obj));
     setOpen(false);
   };
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      const path = event.composedPath();
+      if (!path.includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+    document.body.addEventListener('click', handleClickOutside);
+    return () => document.body.removeEventListener('click', handleClickOutside);
+    
+  }, []);
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
-    import { configureStore } from '@reduxjs/toolkit';
-    import filter from './slices/filterSlice';
-    export const store = configureStore({
-    reducer: { filter },
-    });
+import { configureStore } from '@reduxjs/toolkit';
+import filter from './slices/filterSlice';
+import cart from './slices/cartSlice';
+export const store = configureStore({
+  reducer: { filter, cart },
+});
